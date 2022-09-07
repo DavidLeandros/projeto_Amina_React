7c040e3c64209b6a4fb75f9f1a016e3d3dc29ad1
rodrigox98 <rodrigo.silva31@hotmail.com>
2022-09-07 18:39:28 -0300

    @rodrigox98 [Task:Estilização da página Home] {07/09/2022 18:40}

--- amina/src/App.tsx
+++ amina/src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Navbar2 from './components/estaticos/navbar/Navbar2';
 import './App.css';
 import Login from './paginas/login/Login';
+import Home from './paginas/home/Home';
 
 function App() {
   return (
     <>
-      <Navbar2 />
-      <Login />
+      
+      <Home />
     </>
   );
 }
